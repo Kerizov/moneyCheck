c379167cc2b10a752b7145fe673788d8f8930e08
Kerizov <stepanok980@gmail.com>
2022-02-17 20:49:55 +0300

    moneyCheck v0.0.2

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="wrapper">
     <button type="button" @click="shuffleData">Shuffle</button>
-    <button type="button" @click="addData">addData</button>
-    <button type="button" @click="view">view</button>
-    <my-dialog @create="createData"></my-dialog>
+<!--    <my-dialog @create="createData"></my-dialog>-->
+    <form @submit.prevent>
+      <my-input v-model="labelsValues">label</my-input>
+      <my-input v-model="testValue">value</my-input>
+
+      <button @click="createData">create</button>
+    </form>
     <DoughnutChart
         class="doughnut-chart"
         :data="testData"
@@ -11,37 +15,32 @@
         @chart:render="handleChartRender"
     />
   </div>
+    <div class="chart-btn">
+      <button id="paris" @click="toggleData(0)">first</button>
+      <button id="nîmes" @click="toggleData(1)">second</button>
+      <button id="toulon" @click="toggleData(2)">third</button>
+      <button id="perpignan" @click="toggleData(3)">four</button>
+    </div>
+
 
 </template>
 
 <script>
-import { computed, ref } from "vue";
-import { shuffle } from "lodash";
+import { computed, ref, onMounted } from "vue";
+import { shuffle, concat } from "lodash";
 import { DoughnutChart } from "vue-chart-3";
 
-import MyDialog from "./components/UI/MyDialog";
+// import MyDialog from "./components/UI/MyDialog";
+import MyInput from "./components/UI/MyInput";
 
 export default {
   name: "App",
-  components: { DoughnutChart, MyDialog },
-  data (){
-    return{
-      chartData: [],
-      DialogVisible: false,
-    }
-  },
-  methods: {
-    view(){
-      console.log(this.chartData[0].value);
-    },
-    createData(chartData){
-      this.chartData.push(chartData);
-
-    }
-  },
+  components: { DoughnutChart, MyInput},
   setup() {
-    const dataValues = ref([30, 40, 60, 70, 5]);
-    const labelsValues = ref(["Paris", "Nîmes", "Toulon", "Perpignan", "Autre"]);
+
+    let labelsValues = ref(["Paris", "Nîmes", "Toulon", "Perpignan"]);
+    let testValue = ref([]);
+    let dataValues = ref([30, 40, 60, 70]);
 
     const testData = computed(() => ({
       labels: labelsValues.value,
@@ -53,7 +52,7 @@ export default {
             "#0079AF",
             "#123E6B",
             "#97B0C4",
-            "#A5C8ED",
+
           ],
         },
       ],
@@ -61,36 +60,47 @@ export default {
 
     const options = ref({
       responsive: true,
+      borderWidth: 0,
+      cutout: '80%',
       plugins: {
         legend: {
-          position: "top",
+          // position: "top",
+          display: false
         },
         title: {
-          display: true,
+          display: false,
           text: "Vue-chart-3",
         },
       },
     });
 
+    onMounted(() => {
+      document.getElementById('paris').style.backgroundColor = testData.value.datasets[0].backgroundColor[0];
+      document.getElementById('nîmes').style.backgroundColor = testData.value.datasets[0].backgroundColor[1];
+      document.getElementById('toulon').style.backgroundColor = testData.value.datasets[0].backgroundColor[2];
+      document.getElementById('perpignan').style.backgroundColor = testData.value.datasets[0].backgroundColor[3];
+      document.getElementById('paris').innerText = testData.value.labels[0];
+      document.getElementById('nîmes').innerText = testData.value.labels[1];
+      document.getElementById('toulon').innerText = testData.value.labels[2];
+      document.getElementById('perpignan').innerText = testData.value.labels[3];
+    })
+
+
     function shuffleData() {
       dataValues.value = shuffle(dataValues.value);
     }
-    function addNum(){
-      dataValues.value[0] += 5;
+
+    function createData(){
+      dataValues.value = concat(dataValues.value, testValue.value);
     }
-
-    function addData(){
-      console.log(this.chartData[0].value);
-      // dataValues.value.push(this.chartData[0].value);
+    function toggleData(value){
+      console.log(value);
     }
-
-
-
     function handleChartRender(chart) {
       console.log(chart);
     }
 
-    return { shuffleData, addNum, addData, testData, options, handleChartRender };
+    return { shuffleData, createData, dataValues, toggleData, labelsValues, testValue, testData, options, handleChartRender };
   },
 };
 </script>
@@ -106,10 +116,22 @@ export default {
 }
 .wrapper{
   margin: 0 auto;
-  width: 400px;
+  width: 555px;
   text-align: center;
 }
-.doughnut-chart{
-
+.chart-btn{
+  margin: 0 auto;
+  width: 630px;
+  padding-top: 150px;
+  display: flex;
+  justify-content: space-between;
+}
+.chart-btn button{
+  width: 120px;
+  height: 120px;
+  border-radius: 50%;
+  background-color: red;
+  border: none;
+  font-size: 24px;
 }
 </style>
